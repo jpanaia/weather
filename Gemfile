6c3949b2e03ee2d9f7187a35b3b032c34ac94781
Jaime <jpanaia@gmail.com>
2015-03-01 11:19:16 -0500

    added jquery validation gem

--- Gemfile
+++ Gemfile
@@ -22,13 +22,15 @@ gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
-gem 'wunderground'
+#gem 'wunderground'
 
 gem 'bootstrap-sass', '~> 3.3.3'
 
 gem 'httparty'
 
-gem 'json'
+#gem 'json'
+
+gem 'jquery-validation-rails'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,12 +9,12 @@ class MainController < ApplicationController
   	#@weather_lookup2 = JSON.parse(open("http://api.wunderground.com/api/5f882a1b3107cc1e/conditions/q/OR/Portland.json").read)
 
   	@weather_lookup1 = WeatherLookup.new("OR","Portland")
+    @weather_lookup2 = WeatherLookup.new(params[:state],params[:city])
+    @weather_lookup3 = WeatherLookup.new(params[:state2],params[:city2])
   	@state = params[:state]
   	@city = params[:city]
     @state2 = params[:state2]
     @city2 = params[:city2]
-  	@weather_lookup2 = WeatherLookup.new(params[:state],params[:city])
-  	@weather_lookup3 = WeatherLookup.new(params[:state2],params[:city2])
   end
 
   def reset
